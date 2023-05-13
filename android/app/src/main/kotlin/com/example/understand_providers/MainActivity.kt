8207b76e12605ec50995ee92dec94c7ad2486dab
SAMIULLAH-KALHORO-DEV <samiullahabbasy@gmail.com>
2023-05-14 00:33:01 +0500

    first comment

---
+++ android/app/src/main/kotlin/com/example/understand_providers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.understand_providers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(child: Text("hi")),
+    );
+  }
+}
